add4ae48224c74564e3442ed5ed0b3d394296456
Andrew Camphouse <andrewcamphouse@gmail.com>
2017-04-27 22:23:55 -0500

    Switched to non-working UDP

--- VideoReceiver.py
+++ VideoReceiver.py
@@ -8,45 +8,82 @@ import struct ## new
 HOST='localhost'
 PORT=8080
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 print 'Socket created'
 
-s.bind((HOST,PORT))
+sock.bind((HOST,PORT))
 print 'Socket bind complete'
-s.listen(10)
-print 'Socket now listening'
+##s.listen(10)
+##print 'Socket now listening'
+##
+##conn,addr=s.accept()
+##
+##### new
+##data = ""
+##payload_size = struct.calcsize("L")
+##data = conn.recv(4096)
+##count = 0
+##while True:
+##    if cv2.waitKey(1) & 0xFF == ord('q'):
+##        cap.release()
+##        cv2.destroyAllWindows()
+##        sys.exit(0)
+##    while len(data) < payload_size:
+##        data += conn.recv(4096)
+##    #count += 1
+##    #print "Recieved packet "+str(count)
+##    packed_msg_size = data[:payload_size]
+##    data = data[payload_size:]
+##    msg_size = struct.unpack("L", packed_msg_size)[0]
+##    while len(data) < msg_size:
+##        data += conn.recv(4096)
+##    frame_data = data[:msg_size]
+##    data = data[msg_size:]
+##    ###
+##
+##    frame=pickle.loads(frame_data)
+##
+##    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+##
+##    # Display the resulting frame
+##    #cv2.imshow('frame',gray)
+##    #print frame
+##    cv2.imshow('frame',frame)
+##
+##
+##
 
-conn,addr=s.accept()
 
+
+# UDP:
+print 'Socket bind complete'
 ### new
 data = ""
-payload_size = struct.calcsize("L")
-data = conn.recv(4096)
-count = 0
+
 while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
         sys.exit(0)
-    while len(data) < payload_size:
-        data += conn.recv(4096)
-    #count += 1
-    #print "Recieved packet "+str(count)
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("L", packed_msg_size)[0]
-    while len(data) < msg_size:
-        data += conn.recv(4096)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    ###
+    s = ""
 
-    frame=pickle.loads(frame_data)
+##    while len(data) < payload_size:
+##        data += conn.recv(4096)
+##    #count += 1
+##    #print "Recieved packet "+str(count)
+##    packed_msg_size = data[:payload_size]
+##    data = data[payload_size:]
+    data, addr = sock.recvfrom(48000)
+    s += data
+    if len(s) == (48000*20):
+        frame = np.fromstring(s, dtype=np.uint8)
+        print frame
+       # frame = frame.reshape(480,640,3)
+        s = ""
+        cv2.imshow('frame',frame)
 
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+   # msg_size = struct.unpack("L", packed_msg_size)[0]
+    
 
-    # Display the resulting frame
-    #cv2.imshow('frame',gray)
-    #print frame
-    cv2.imshow('frame',frame)
+
--- VideoSender.py
+++ VideoSender.py
@@ -1,15 +1,43 @@
 import cv2
 import numpy as np
 import socket
+import gzip
 import sys
 import pickle
 import struct ### new code
+IP = "localhost"
+PORT = 8080
+
+# Not sure why video isn't showing up
+
 cap=cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8080))
+##cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 4)
+##cap.set(cv2.CAP_PROP_FRAME_WIDTH, 4)
+##print cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+##print cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+clientsocket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+#clientsocket.connect(('localhost',8080))
 while True:
+##    ret,frame=cap.read()
+##    data = pickle.dumps(frame) ### new code
+##    clientsocket.send(struct.pack("L", len(data))+data)
+##    #clientsocket.sendall(struct.pack("L", len(data))+data) ### new code
+    
+    # UDP:
     ret,frame=cap.read()
-    data = pickle.dumps(frame) ### new code
-    clientsocket.send(struct.pack("L", len(data))+data)
+    d = frame.flatten()
+    dat1 = sys.getsizeof(frame)
+    dat2 = sys.getsizeof(d)
+    s = d.tostring()
+    dat3 = sys.getsizeof(s)
+    print dat1
+    print dat2
+    print "Dat3: "+str(dat3)
+    for i in xrange(20):
+        clientsocket.sendto(s[i*48000:(i+1)*48000],(IP, PORT))
+    #data = pickle.dumps(frame) ### new code
+    #dataSize = sys.getsizeof(data)
+    #print dataSize
+    #a = clientsocket.sendto(struct.pack("L", len(data))+data, (IP, PORT))
     #clientsocket.sendall(struct.pack("L", len(data))+data) ### new code
     
